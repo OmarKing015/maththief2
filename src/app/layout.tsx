e5a2f8fdb2b55928ef8378460121bb802b0473a5
Omar Osama <66084757+OmarKing015@users.noreply.github.com>
2024-02-16 22:47:34 +0000

    speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-
-
+import {SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children} <Analytics/></body>
+      <body className={inter.className}>{children} <SpeedInsights /> <Analytics/></body>
     </html>
   );
 }
