2606e502d525d0172dac4acbe473929b3fa2907b
Omar Osama <66084757+OmarKing015@users.noreply.github.com>
2024-03-03 21:27:54 +0200

    Error in applied math json file

--- src/app/appliedmath/data/data.ts
+++ src/app/appliedmath/data/data.ts
@@ -54,31 +54,7 @@ export const myData: SessionsData = {
         {
           name: "PDF",
           link: "https://omar-sherbeni.com/student/G2bMQTPtMV3KGLojM7rE/uploads/files/42585_S2%20mechanics-1_merged.pdf",
-        }, {
-          session: "Motion of body in St. line",
-          lectures: [
-            {
-              name: "Explanation Part 1",
-              link: "https://iframe.mediadelivery.net/play/33074/b3499da2-c3bb-4226-827f-96a5d5f34939",
-            },
-            {
-              name: "Explanation Part 2",
-              link: "https://iframe.mediadelivery.net/play/33074/f6028548-2946-47ea-984a-f7f1a834e5e8",
-            },
-            {
-              name: "Classwork",
-              link: "https://iframe.mediadelivery.net/play/33074/4b9a15cb-259e-4fa7-b94d-ca99a705a661",
-            },
-            {
-              name: "Homework",
-              link: "https://iframe.mediadelivery.net/play/33074/d87d3cd4-6d02-42c4-a8dd-41602c63699e",
-            },
-            {
-              name: "PDF",
-              link: "https://omar-sherbeni.com/student/G2bMQTPtMV3KGLojM7rE/uploads/files/88975_S1%20mechanics-1_merged.pdf",
-            },
-          ],
-        }
+        }, 
       ],
     }, {
       session: "RECTILIEAR MOTION",
